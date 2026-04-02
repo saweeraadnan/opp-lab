eca815b8bc55451028fc866549f0b9ea3ece18c8
Saweera Adnan <saweeraadnan528@gmail.com>
2026-04-02 18:37:09 +0500

    Add files via upload
    🔹 Experiment 19
    Class with Constructor Displaying Message

---
+++ experiment19.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Demo {
+public:
+    Demo() {
+        cout << "Object Created!\n";
+    }
+};
+
+int main() {
+    Demo d1, d2, d3;
+
+    return 0;
+}
